bdf763d1c46dbb5384a1b34502d1b9f7931af263
Pete <pmiglio19@yahoo.com>
2022-04-04 20:26:58 -0400

    Slime is a-bouncing

--- 528_Final_Project/Assets/Scripts/EnemyCharacters/Slime.cs
+++ 528_Final_Project/Assets/Scripts/EnemyCharacters/Slime.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.EnemyCharacters
 {
     class Slime : BaseEnemy
     {
+        float maxMoveDistance = .5f;
+        //Set this to your objects initial position when game starts.
+        Vector3 origin;
+        float speed = 5;
+
         public Slime() : base()
         {
             health = new Health(5);
@@ -16,7 +22,9 @@ namespace Assets.Scripts.EnemyCharacters
 
         private void Update()
         {
-
+            Vector3 destination = transform.position;
+            destination.y = (transform.position.y > origin.y + maxMoveDistance) ? origin.y : origin.y + maxMoveDistance;
+            transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
         }
 
         private void FixedUpdate()
