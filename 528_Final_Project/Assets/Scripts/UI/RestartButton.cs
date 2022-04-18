8a6786b896921946fecf5c28a728077d9227aebc
Pete <pmiglio19@yahoo.com>
2022-04-18 19:27:33 -0400

    Restart and quit buttons

---
+++ 528_Final_Project/Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class RestartButton : MonoBehaviour
+    {
+        private Button button;
+        public InventoryPanel inventoryPanel;
+
+        private void Start()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(TaskOnClick);
+        }
+
+        private void TaskOnClick()
+        {
+            SceneManager.LoadScene("OverworldScene");
+        }
+    }
+}
