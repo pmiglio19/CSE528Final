1b11b8e76df0e4ae48513e4974bed5514bda723d
Pete <pmiglio19@yahoo.com>
2022-04-22 08:45:10 -0400

    Added strength and xp to ui

--- 528_Final_Project/Assets/Scripts/EntityMechanics/Experience.cs
+++ 528_Final_Project/Assets/Scripts/EntityMechanics/Experience.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Assets.Scripts.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,18 +13,22 @@ namespace Assets.Scripts.EntityMechanics
         private int experienceAmount;
         private int experienceLevel;
         private int nextExperienceLevelGoal;
+        private ExperienceUI experienceUI;
+        private StrengthUI strengthUI;
 
         public Experience()
         {
             experienceAmount = 0;
             experienceLevel = 1;
             nextExperienceLevelGoal = 10;
+            experienceUI = new ExperienceUI();
         }
 
         public void IncrementExperience(int amount, DamageDealt damage)
         {
             experienceAmount += amount;
             CheckIfLevelUp(experienceAmount, damage);
+            
         }
 
         private void CheckIfLevelUp(int xpAmount, DamageDealt damage)
@@ -34,6 +39,9 @@ namespace Assets.Scripts.EntityMechanics
                 nextExperienceLevelGoal += 10;
 
                 damage.ChangeMultiplier(damage.GetMultiplier() + 1);
+
+                experienceUI.IncrementExperience(1);
+                strengthUI.IncrementStrength(1);
             }
         }
     }
---
+++ 528_Final_Project/Assets/Scripts/UI/StrengthUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.EntityMechanics;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class StrengthUI : MonoBehaviour
+    {
+        private static int currentStrength = 1;
+        public Text textStrength;
+
+        void Start()
+        {
+            textStrength.text = currentStrength.ToString();
+        }
+
+        void Update()
+        {
+            textStrength.text = currentStrength.ToString();
+        }
+
+        public void IncrementStrength(int amount)
+        {
+            currentStrength = currentStrength + amount;
+        }
+
+        public void DecrementStrength(int amount)
+        {
+            currentStrength = currentStrength - amount;
+        }
+
+        public void ResetStrength()
+        {
+            currentStrength = 1;
+        }
+    }
+}
